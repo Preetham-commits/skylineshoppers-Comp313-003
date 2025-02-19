dfff6963f2ff425f2be2c177cba35a327e919e3b
Mohitsh45 <mohitsharma1077@outlook.com>
2025-02-19 16:05:39 -0500

    Product list, edit, create screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,21 +6,42 @@ import Home from "./pages/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Dashboard from "./components/Dashboard";
+import ProductScreen from './screens/ProductScreen'
+import ProductListScreen from './screens/ProductListScreen'
+import ProductEditScreen from './screens/ProductEditScreen'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <AuthProvider>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          
+          <Route path='/product/:id' component={ProductScreen} />
+		  <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
-  );
+          <Route
+            path='/admin/productlist'
+            component={ProductListScreen}
+            exact
+          />
+          <Route
+            path='/admin/productlist/:pageNumber'
+            component={ProductListScreen}
+            exact
+          />
+          <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  )
 };
 
 export default App;
