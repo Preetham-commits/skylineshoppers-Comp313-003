bfe7e2f10518d8ab692156c58138897a71c0a0e5
Mohitsh45 <mohitsharma1077@outlook.com>
2025-03-10 13:32:10 -0400

    Merge conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,42 +6,34 @@ import Home from "./pages/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Dashboard from "./components/Dashboard";
-import ProductScreen from './screens/ProductScreen'
-import ProductListScreen from './screens/ProductListScreen'
-import ProductEditScreen from './screens/ProductEditScreen'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { Container } from 'react-bootstrap'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import ProductScreen from './screens/ProductScreen';
+import ProductListScreen from './screens/ProductListScreen';
+import ProductEditScreen from './screens/ProductEditScreen';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <Router>
       <Header />
-      <main className='py-3'>
+      <main className="py-3">
         <Container>
-          
-          <Route path='/product/:id' component={ProductScreen} />
-		  <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route
-            path='/admin/productlist'
-            component={ProductListScreen}
-            exact
-          />
-          <Route
-            path='/admin/productlist/:pageNumber'
-            component={ProductListScreen}
-            exact
-          />
-          <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
+          <Routes>  {/* ✅ Wrap all Route components inside <Routes> */}
+            <Route path="/product/:id" element={<ProductScreen />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/admin/productlist" element={<ProductListScreen />} />
+            <Route path="/admin/productlist/:pageNumber" element={<ProductListScreen />} />
+            <Route path="/admin/product/:id/edit" element={<ProductEditScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
     </Router>
-  )
+  );
 };
 
 export default App;
